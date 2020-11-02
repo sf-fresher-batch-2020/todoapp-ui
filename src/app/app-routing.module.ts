ef492f2ce800de075e46343f353e550555013ad2
Pavan Manikanta Kiran Maddu <m.p.m.kiran@gmail.com>
2020-11-02 19:38:08 +0530

    components created! routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
+import { ProfileComponent } from './profile/profile.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', component: WelcomeComponent },
+  { path: 'register', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  SignupComponent,
+  WelcomeComponent,
+  LoginComponent,
+  DashboardComponent,
+  ProfileComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+// import { WelcomeComponent } from './welcome/welcome.component';
+// import { LoginComponent } from './login/login.component';
+// import { SignupComponent } from './signup/signup.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    routingComponents,
+    ProfileComponent
+    // WelcomeComponent,
+    // LoginComponent,
+    // SignupComponent,
+    // DashboardComponent
   ],
   imports: [
     BrowserModule,
