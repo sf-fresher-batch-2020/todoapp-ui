b6c317e05223c40f8a15512f4f96f5d588650c87
Pavan Manikanta Kiran Maddu <m.p.m.kiran@gmail.com>
2020-11-03 10:03:43 +0530

    branch issue1

---
+++ services/authServices.js
@@ -0,0 +1,29 @@
+class AuthServices {
+    register(user_data) {
+        var temp = JSON.parse(localStorage.getItem("USERS"));
+        var users = temp ? temp : [];
+        users.push(user_data);
+        localStorage.setItem("USERS", JSON.stringify(users));
+    }
+
+    login(email, password) {
+        var temp = JSON.parse(localStorage.getItem("USERS"));
+        for (var i = 0; i < temp.length; i++) {
+            if (temp[i].email == email) {
+                if (temp[i].password == password) {
+                    localStorage.setItem("LOGGED_IN_USER", JSON.stringify({
+                        'name': temp[i].name,
+                        'email': temp[i].email
+                    }));
+                    $('#loggedin').show();
+                    $('#loggedout').hide();
+                    $("#content").load('../views/dashboard.html');
+                } else {
+                    throw "Password incorrect..!";
+                }
+            } else {
+                throw "Email not registered..!";
+            }
+        }
+    }
+}
